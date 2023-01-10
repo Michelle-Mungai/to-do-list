c9bb1b5b038b6ed9a743f31954505e2716f72356
Michelle_Mungai <chellymungai@gmail.com>
2023-01-10 14:09:15 +0300

    imports TodoList to Home component

---
+++ src/views/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoList from '../components/TodoList'
+
+function Home() {
+  return (
+    <div>Home
+        <TodoList />
+
+    </div>
+  )
+}
+
+export default Home
