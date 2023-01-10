64ee3e67c6d9350c2f5b4ce6090475eebf04b567
Michelle_Mungai <chellymungai@gmail.com>
2023-01-10 11:57:46 +0300

    Unchanged to-do list code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Capturing the main entry point of the file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
